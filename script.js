539889c371635cdf1b704aac35512a7c9d6fbc63
dantascrispim <114705745+dantascrispim@users.noreply.github.com>
2023-05-10 14:40:38 -0300

    upload do site verificador de idade

---
+++ script.js
@@ -0,0 +1,38 @@
+function verificar() {
+    var data = new Date()
+    var ano = data.getFullYear()
+    var fano = document.getElementById('txtano')
+    var res = document.querySelector('div#res')
+    if (fano.value.length == 0 || Number(fano.value) > ano) { window.alert('[ERRO] verifique os dados e tente novamente!')
+    } else {
+        var fsex = document.getElementsByName('radsex')
+        var idade = ano - Number(fano.value)
+        var gênero = ''
+        var img = document.createElement('img')
+        img.setAttribute('id', 'foto')
+        if (fsex[0].checked) {
+            gênero = 'Homem'
+            if(idade >=0 && idade <10){
+                img.setAttribute('src','foto-bebe-m.jpg.jpg')
+            }else if (idade <21)
+            {img.setAttribute('src','foto-jovem-m.png.png')
+            }else if (idade <50)
+            {img.setAttribute('src','foto-adulto-m.png')
+            }else{img.setAttribute('src','foto-idoso-m.png')
+
+            }
+        }else if (fsex[1].checked) {
+            gênero ='Mulher'
+            if(idade >=0 && idade <10){
+                img.setAttribute('src','foto-bebe-f.png')
+            }else if (idade <21){img.setAttribute('src','foto-jovem-f.png')
+            }else if (idade <50){img.setAttribute('src','foto-adulto-f.png')
+            }else{img.setAttribute('src','foto-idoso-f.png')
+            }
+        } 
+        res.innerHTML = `Detectamos ${gênero} com ${idade} anos`
+        res.appendChild(img)
+    }
+}
+
+
